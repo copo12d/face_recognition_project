fc1f91899c7608806169e05feb8e5ea493e69846
Alepex18 <ale.arteaga.18.09@gmail.com>
2025-04-08 14:56:37 -0400

    fix: optimized face recognition by processing every other frame

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ detected_names, detected_faces = load_known_faces()
 # Iniciar la captura de video
 video_capture = cv2.VideoCapture(0)
 
+process_this_frame = True
+face_locations = []
+recognized_faces = []
+
 while True:
     ret, frame = video_capture.read()
     if not ret:
@@ -16,25 +20,41 @@ while True:
 
     # Convertir el frame a RGB
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # Actualizar la posición de las caras en cada frame
     face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        matches = face_recognition.compare_faces(detected_faces, face_encoding)
-        name = "Desconocido"
+    # Realizar reconocimiento facial cada otro frame
+    if process_this_frame:
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+        recognized_faces = []
 
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = detected_names[first_match_index]
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(detected_faces, face_encoding)
+            name = "Desconocido"
 
-        # Dibujar un rectángulo y mostrar el nombre
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = detected_names[first_match_index]
+
+            recognized_faces.append(name)
+
+    # Dibujar los rectángulos y nombres en cada frame
+    for (top, right, bottom, left), name in zip(face_locations, recognized_faces):
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.putText(frame, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
-    cv2.imshow('Reconocimiento Facial', frame)
+    # Alternar el booleano para procesar cada otro frame
+    process_this_frame = not process_this_frame
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Mostrar el frame en la ventana
+    window_name = 'Reconocimiento Facial'
+    cv2.imshow(window_name, frame)
+
+    # Verificar si la ventana fue cerrada o si se presionó 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) < 1:
         break
 
+# Liberar recursos
 video_capture.release()
 cv2.destroyAllWindows()
